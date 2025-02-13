47f28ee7006af73cb1d076af6709865f5c2f84b9
fanwwi <farangiz.anvarjanova553@gmail.com>
2025-02-13 23:11:51 +0600

    .

--- api/app.js
+++ api/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const axios = require("axios");
 const cron = require("node-cron");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 const API_URL = "https://api.nasa.gov/DONKI/FLR";
-const API_KEY = "DEMO_KEY"; 
+const API_KEY = "DEMO_KEY";
 
 const params = {
   startDate: "2024-02-01",
@@ -16,6 +17,10 @@ const params = {
 
 let solarData = [];
 
+// Разрешаем CORS
+app.use(cors());
+
+// Функция для получения данных с API
 const getData = async () => {
   try {
     const response = await axios.get(API_URL, { params });
@@ -31,6 +36,7 @@ const getData = async () => {
   }
 };
 
+// Роут для получения данных
 app.get("/solar-flare-data", (req, res) => {
   if (solarData.length > 0) {
     res.json(solarData);
@@ -39,8 +45,10 @@ app.get("/solar-flare-data", (req, res) => {
   }
 });
 
+// Крон-задание для обновления данных каждые 3 часа
 cron.schedule("0 0 */3 * *", getData);
 
+// Запуск сервера и загрузка данных при старте
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
   getData();
